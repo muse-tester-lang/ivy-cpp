8de80b9f8bcbb0f30de54f3faeb808ec5491807a
muse-tester-lang <63867960+muse-tester-lang@users.noreply.github.com>
2020-05-26 09:27:12 -0400

    Update ivy-c_install.sh

--- ivy-c_install.sh
+++ ivy-c_install.sh
@@ -2,7 +2,7 @@ sudo apt-get install libpcre3-dev tk-dev libglib2.0-dev libxt-dev
 
 V=3.15.1
 TARBALL=ivy-c_${V}.tar.xz
-curl -OL http://www.eei.cena.fr/products/ivy/download/packages/${TARBALL}
+curl -OL https://www.eei.cena.fr/products/ivy/download/packages/${TARBALL}
 tar xvf ${TARBALL}
 cd ivy-c_${V}/src
 make
