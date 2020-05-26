158ed4a0bb50ae7f785b2344d8829d08424af327
Yizhao Lang <yizhao@muse.dev>
2020-05-26 10:08:19 -0400

    Try to not use sudo

--- ivy-c_install.sh
+++ ivy-c_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-sudo apt-get install libpcre3-dev tk-dev libglib2.0-dev libxt-dev 
+# sudo apt-get install libpcre3-dev tk-dev libglib2.0-dev libxt-dev 
 
 V=3.15.1
 TARBALL=ivy-c_${V}.tar.xz
@@ -7,4 +7,5 @@ curl -OL https://www.eei.cena.fr/products/ivy/download/packages/${TARBALL}
 tar xvf ${TARBALL}
 cd ivy-c_${V}/src
 make
-sudo make install
+make install
+# sudo make install
